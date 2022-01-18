6a83980aa85ea40e1e4dcaae7af77569e25d18de
justin-foster-91 <55926012+justin-foster-91@users.noreply.github.com>
2022-01-18 13:54:25 -0800

    Hangar and Templates have basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import templates from './shipTemplates';
 
 function App() {
+  const [userShips, setUserShips] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="Routes">
+          <Routes>
+            <Route index 
+              element={<HangarPage
+                userShips={userShips}
+                setUserShips={setUserShips} 
+              />}
+            />
+            <Route path={'hangar'} 
+              element={<HangarPage 
+                userShips={userShips}
+                setUserShips={setUserShips} 
+              />}
+            />
+            <Route path={'templates'} 
+              element={<TemplatePage
+                userShips={userShips}
+                setUserShips={setUserShips} 
+              />}
+            />
+          </Routes>
+      </main>
+    </div>
+  );
+}
+
+const HangarPage = (props) => {
+  return (
+    <div className="hangarDisplay">
+      Hangar Page
+      {console.log(props.userShips)}
+      <p></p>
+
+      {props.userShips.map((ship) => {return ship.shipName})}
+      
+      <p></p>
+      <Link to='/templates' >
+        <button>Templates</button>
+      </Link>
+    </div>
+  );
+}
+
+const TemplatePage = (props) => {
+  const [selectedTemplate, setSelectedTemplate] = useState({});
+
+  return (
+    <div className="templateDisplay">
+      Template Page
+      {console.log(props.userShips)}
+      <p></p>
+
+      {
+        templates.map((template, idx) => {
+          return <button onClick={() => setSelectedTemplate(template)} value={template.shipName} key={idx}>{template.shipName || 'Template'} </button>
+        })
+      }
+
+      <p></p>
+
+      {selectedTemplate.shipName ?
+        <div className="templateRender">
+          {selectedTemplate.shipName} (Tier {selectedTemplate.tierId} {selectedTemplate.frameId})
+        </div>
+      : <div></div>}
+
+      <p></p>
+
+      <Link to="/hangar">
+        <button>Cancel</button>
+        <button onClick={() => props.setUserShips((userShips) => userShips.concat([selectedTemplate]))}>Select</button>
+      </Link>
+      
     </div>
   );
 }
